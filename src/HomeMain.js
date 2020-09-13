6ef9bea0353e73d32b1a0f144532da5c52af061f
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-09-13 02:24:01 -0500

    notes mapped to Home main section

--- src/HomeMain.js
+++ src/HomeMain.js
@@ -1,9 +1,20 @@
 import React from'react';
+import dummyStore from './dummy-store';
 
 class HomeMain extends React.Component {
   render() {
-    return (
-      <p>Home Main</p>
+    const notes = dummyStore.notes.map(note => {
+      return (
+        <div className="note" key={note.id}>
+          <h3>{note.name}</h3>
+          <p>Date modified on {note.modified}</p>
+        </div>
+      )
+    });
+  return (
+    <>
+      {notes}
+    </>
     );
   }
 }
--- src/HomeSidebar.js
+++ src/HomeSidebar.js
@@ -9,7 +9,6 @@ class HomeSidebar extends React.Component {
       )
     });
   return (
-    // {folders}
     <div>
       {folders}
     </div>
