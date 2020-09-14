a81502b5aeaf016f256eeb456a31a80a5ac7e71f
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-09-14 12:58:42 -0500

    added NavLink components to mapped folders in HomeSidebar

--- src/HomeSidebar.js
+++ src/HomeSidebar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import dummyStore from './dummy-store';
 
 class HomeSidebar extends React.Component {
   render() {
     const folders = dummyStore.folders.map(folder => {
       return (
-      <div className="folder" key={folder.id}>{folder.name}</div>
+        <NavLink to={`/folder/${folder.id}`}>
+          <div className="folder" key={folder.id}>{folder.name}</div>
+        </NavLink>
       )
     });
   return (
