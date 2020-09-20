ce8e51d63586d1ecb5a8636c543a54ede474cfc2
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-09-20 14:16:14 -0500

    note page now displaying selected notes content

--- src/NoteMain.js
+++ src/NoteMain.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import ApiContext from './ApiContext';
+import Note from './Note';
 
 class NoteMain extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {}
+    }
+  }
+  static contextType = ApiContext;
+
   render() {
+    const { noteId } = this.props.match.params;
+    const { notes=[] } = this.context;
+    const findNote = (notes=[], noteId) =>
+      notes.find(note => note.id === noteId);
+    const note = findNote(notes, noteId) || { content: '' };
+    console.log(note);
+
+
     return (
-      <p>Note Main</p>
+      <section className='NoteMain'>
+        <Note
+          id={note.id}
+          name={note.name}
+          modified={note.modified}
+        />
+        <div className='NoteMain-content'>
+          {note.content}
+        </div>
+      </section>
     );
   }
 }
