36c88ccf4aa97b46f12f7655ab12664bc9d8edc7
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-09-20 21:01:06 -0500

    Note page sidebar now displays current folder

--- src/NoteSidebar.js
+++ src/NoteSidebar.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import ApiContext from './ApiContext';
 
 class NoteSidebar extends React.Component {
+  static defaultProps = {
+    match: {
+      params: {}
+    }
+  }
+  static contextType = ApiContext;
+
+
   render() {
+    
+    const findFolder = (folders=[], folderId) =>
+      folders.find(folder => folder.id === folderId);
+    const findNote = (notes=[], noteId) =>
+      notes.find(note => note.id === noteId);
+    const { notes, folders, } = this.context;
+    const { noteId } = this.props.match.params;
+    const note = findNote(notes, noteId) || {};
+    const folder = findFolder(folders, note.folderId);
+
     return (
       <div className="NoteSidebar">
         <button>Back</button>
-        <p>Shows only the name of the active folder</p>
+        {/* {folder && (<h3>{folder.name}</h3>)} */}
+        {/*  optional chaining */}
+        <h3>{folder?.name}</h3>
       </div>
     );
   }
